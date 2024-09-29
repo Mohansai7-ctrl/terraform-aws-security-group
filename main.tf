11f65b5633856144ea3913510cff661c8c0d53f3
Mohansai <bandarumohansai@gmail.com>
2024-09-29 22:53:16 +0530

    practiced vpc infra creation

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@ resource "aws_security_group" "expense_sg" {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
-    
+
     tags = merge(
         var.common_tags,
         var.sg_tags,
